a300209167ba28d0ce588f60dc54cb8a04e0a288
yukinakamurakero <yuki0823tibid@icloud.com>
2023-08-02 22:13:14 +0900

    t

--- dep/dep/settings.py
+++ dep/dep/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d^ok$ug7g%2r9_y#2)mga$inr0b-fz6x6d+4^o=z4rdh4-ni=^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
